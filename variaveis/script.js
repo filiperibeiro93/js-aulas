2e2e364dce42d17d267fd2b8036d776b609120b9
Filipe Ribeiro <facribeiro.93@gmail.com>
2024-05-30 20:42:40 -0300

    treinando-variaveis

---
+++ variaveis/script.js
@@ -0,0 +1,15 @@
+var nome = 'Filipe';
+var idade = 31;
+
+console.log(nome, idade);
+
+var sobrenome = 'Ribeiro', 
+  cidade = 'Recife';
+
+console.log(sobrenome, cidade);
+
+var semDefinir;
+console.log(semDefinir);
+
+var comidaFavorita;
+comidaFavorita = 'Lasanha';
