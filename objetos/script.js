22fe6b519f751f0c18b198909a9dfee000f90846
Filipe Ribeiro <facribeiro.93@gmail.com>
2024-05-31 14:00:52 -0300

    exercicios

--- objetos/script.js
+++ objetos/script.js
@@ -23,3 +23,33 @@ menu.color = 'blue';
 
 menu.hasOwnProperty('width') // true
 menu.hasOwnProperty('height') // false
+
+// Crie um objeto com os seus dados pessoais
+// Deve possuir pelo menos duas propriedades nome e sobrenome
+var eu = {
+  nome: 'Filipe',
+  sobrenome: 'Ribeiro',
+  idade: 31,
+}
+// Crie um método no objeto anterior, que mostre o seu nome completo
+eu.nomeCompleto = function() {
+  return `${this.nome} ${this.sobrenome}`;
+}
+// Modifique o valor da propriedade preco para 3000
+var carro = {
+  preco: 1000,
+  portas: 4,
+  marca: 'Audi',
+}
+carro.preco = 3000;
+
+// Crie um objeto de um cachorro que represente um labrador,
+// preto com 10 anos, que late ao ver um homem
+var cachorro = {
+  raca: 'labrador',
+  cor: 'preto',
+  idade: 10,
+  verHomem() {
+    console.log('Latiu');
+  },
+}
