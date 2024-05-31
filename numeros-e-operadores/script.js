096641c9e838a1af64dc7a4c8981d85196898a01
Filipe Ribeiro <facribeiro.93@gmail.com>
2024-05-30 21:03:58 -0300

    numeros-e-operadores

---
+++ numeros-e-operadores/script.js
@@ -0,0 +1,24 @@
+
+var gols = 1000;
+var frase1 = 'Romário fez ' + gols + ' gols';
+var frase2 = `Romário fez ${gols} gols`; // Utilizando Template String
+console.log(frase2);
+
+// Declare uma variável contendo uma string
+var teste = 'teste';
+// Declare uma variável contendo um número dentro de uma string
+var numero = 'teste2teste';
+// Declare uma variável com a sua idade
+var idade = 31;
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+var nome = 'Filipe';
+var sobrenome = 'Ribeiro';
+var nomeCompleto = nome + ' ' + sobrenome;
+nomeCompleto = `${nome} ${sobrenome}`
+console.log(nomeCompleto);
+// Coloque a seguinte frase em uma variável: It's time
+var teste2 = "It's time";
+console.log(teste2);
+// Verifique o tipo da variável que contém o seu nome
+console.log(typeof nome);
