c971b88b2c2cbe57f346cae6b32e909ae29748ea
Filipe Ribeiro <facribeiro.93@gmail.com>
2024-05-31 14:18:11 -0300

    exercicios

---
+++ tudo-e-objeto/script.js
@@ -0,0 +1,17 @@
+var btn = document.querySelector('.btn');
+
+// nomeie 3 propriedades ou métodos de strings
+var nome = 'Filipe';
+nome.charAt(2);
+nome.length;
+nome.concat(' Ribeiro');
+console.log(nome);
+// nomeie 5 propriedades ou métodos de elementos do DOM
+document.addEventListener
+document.append
+btn.classList
+btn.addEventListener
+btn.appendChild
+// busque na web um objeto (método) capaz de interagir com o clipboard, 
+// clipboard é a parte do seu computador que lida com o CTRL + C e CTRL + V
+// clipboard js
