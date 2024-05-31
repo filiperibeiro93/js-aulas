91abfc38d8955d6ee987cc7a587f622c46d63e67
Filipe Ribeiro <facribeiro.93@gmail.com>
2024-05-31 14:55:57 -0300

    iterando-arrays

--- arrays-e-loops/script.js
+++ arrays-e-loops/script.js
@@ -1,11 +1,11 @@
-var videoGames = ['Switch', 'PS4', 'XBox'];
+var videoGames = ['Switch', 'PS4', 'XBox', '3DS'];
 
 videoGames[0] // Switch
 videoGames[2] // Xbox
 
-videoGames.pop();
-videoGames.push('3DS');
-videoGames.length;
+// videoGames.pop();
+// videoGames.push('3DS');
+// videoGames.length;
 
 for (var numero = 0; numero <= 10; numero++) {
   console.log(numero);
@@ -15,4 +15,27 @@ var i = 0;
 while (i <= 10) {
   console.log(i);
   i++;
+}
+
+for (var i = 0; i < videoGames.length; i++) {
+  console.log(videoGames[i]);
+}
+
+for (var i = 0; i < videoGames.length; i++) {
+  console.log(videoGames[i]);
+  if (videoGames[i] === 'PS4')  {
+    break;
+  }
+}
+
+var frutas = ['Banana', 'Maçã', 'Pera', 'Uva', 'Melância']
+frutas.forEach(function(fruta, index, array) {
+  console.log(fruta, index, array);
+});
+
+var numero = 0;
+var maximo = 50;
+for(;numero < maximo;) {
+  console.log(numero);
+  numero++;
 }
