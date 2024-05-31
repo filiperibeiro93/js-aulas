df5ba0518a5795d350de118d0889c0e413e56306
Filipe Ribeiro <facribeiro.93@gmail.com>
2024-05-31 11:32:40 -0300

    exercicios

--- boolean-e-condicionais/script.js
+++ boolean-e-condicionais/script.js
@@ -27,3 +27,46 @@ switch (corFavorita) {
   default:
     console.log('Feche os olhos');
 }
+
+// Verifique se a sua idade é maior do que a de algum parente
+// Dependendo do resultado coloque no console 'É maior', 'É igual' ou 'É menor'
+var minhaIdade = 31;
+var idadeMae = 56;
+
+if (minhaIdade < idadeMae) {
+  console.log('É menor');
+} else if (minhaIdade === idadeMae) {
+  console.log('É igual')
+} else {
+  console.log('É maior')
+}
+
+// Qual valor é retornado na seguinte expressão?
+var expressao = (5 - 2) && (5 - ' ') && (5 - 2);
+console.log(expressao);
+
+// Verifique se as seguintes variáveis são Truthy ou Falsy
+var nome = 'Andre';
+console.log(!!nome);
+var idade = 28;
+var possuiDoutorado = false;
+var empregoFuturo;
+var dinheiroNaConta = 0;
+
+// Compare o total de habitantes do Brasil com China (valor em milhões)
+var brasil = 207;
+var china = 1340;
+
+// O que irá aparecer no console?
+if(('Gato' === 'gato') && (5 > 2)) {
+  console.log('Verdadeiro');
+} else {
+  console.log('Falso');
+}
+
+// O que irá aparecer no console?
+if(('Gato' === 'gato') || (5 > 2)) {
+  console.log('Gato' && 'Cão');
+} else {
+  console.log('Falso');
+}
