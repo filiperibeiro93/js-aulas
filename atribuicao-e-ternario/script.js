93940c2f142c6a13a7ea9c31852516c38d3277f2
Filipe Ribeiro <facribeiro.93@gmail.com>
2024-05-31 15:12:28 -0300

    ternarios

---
+++ atribuicao-e-ternario/script.js
@@ -0,0 +1,20 @@
+var numero = 20;
+
+numero += 10;
+console.log(numero);
+
+var idade = 19;
+var podeBeber = (idade >= 18) ? 'Pode beber' : 'Não pode beber';
+console.log(podeBeber) // Pode beber
+
+// condição ? true : false
+
+var possuiFaculdade = true;
+if(possuiFaculdade) console.log('Possui faculdade');
+else console.log('Não possui faculdade');
+
+// ou
+if(possuiFaculdade)
+  console.log('Possui faculdade');
+else
+  console.log('Não possui faculdade');
