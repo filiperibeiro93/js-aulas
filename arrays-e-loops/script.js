12ad528075110ef338c1146a1ececc57dcfc9dee
Filipe Ribeiro <facribeiro.93@gmail.com>
2024-05-31 14:39:37 -0300

    loops

---
+++ arrays-e-loops/script.js
@@ -0,0 +1,18 @@
+var videoGames = ['Switch', 'PS4', 'XBox'];
+
+videoGames[0] // Switch
+videoGames[2] // Xbox
+
+videoGames.pop();
+videoGames.push('3DS');
+videoGames.length;
+
+for (var numero = 0; numero <= 10; numero++) {
+  console.log(numero);
+}
+
+var i = 0;
+while (i <= 10) {
+  console.log(i);
+  i++;
+}
